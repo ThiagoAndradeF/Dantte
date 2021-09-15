da5fb66bf060dd01e01731bb3afa0a3222bd9c47
DantteOwO <90792084+DantteOwO@users.noreply.github.com>
2021-09-15 14:52:30 -0300

    Add files via upload

---
+++ Projeto/easy/data.js
@@ -0,0 +1,9 @@
+window.audios = [
+  {
+    title: "Take Over",
+    artist: "Diego Fernandes",
+    cover: "take-over.jpg",
+    file: "Take Over (ft. Jeremy McKinnon (A Day To Remember), MAX, Henry) Worlds 2020 - League of Legends_160k.mp3"
+  },
+ 
+];
